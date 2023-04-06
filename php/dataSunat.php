66cc102f823566caad501560f358bee5a9bb0b79
Carlos Pariona <infocat2.0@gmail.com>
2023-04-06 16:10:54 -0500

    1.57b

--- php/dataSunat.php
+++ php/dataSunat.php
@@ -1,24 +1,28 @@
 <?php
 include "conexion.php";
 
+$fila = array();
 $sql="SELECT * FROM `clientes`
 where cliRuc = '{$_POST['ruc']}' and cliActivo =1";
 $resultado=$cadena->query($sql);
 if($resultado->num_rows>=1){
-	$fila = array();
 
 	$row=$resultado->fetch_assoc();
 	$fila = array(
 		"razon_social" => $row['cliRazonSocial'],
 		"domicilio_fiscal" => $row['cliDomicilio']
 	);
-	echo json_encode($fila);
 
 }else{
-	$json = file_get_contents("https://infocatsoluciones.com/app/consorcioSoriano/dataSunat.php?ruc={$_POST['ruc']}");
+	/* $json = file_get_contents("https://infocatsoluciones.com/app/consorcioSoriano/dataSunat.php?ruc={$_POST['ruc']}");
 	$obj = json_decode($json);
 	//echo $obj->access_token;
 	//var_dump($obj->ruc);
-	echo json_encode($obj);
+	echo json_encode($obj); */
+	$fila = array(
+		"razon_social" => '',
+		"domicilio_fiscal" => ''
+	);
 }
+echo json_encode($fila);
 ?>
